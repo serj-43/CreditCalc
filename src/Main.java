76403f4d1dcf4b28ba99eb53c19ae09e2bb8eced
serj-43 <serj-43@yandex.ru>
2022-04-30 20:45:30 +0300

    add cycle

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        System.out.println("Ваш платеж: " + service.calculate(1, 1000_000, 9.99));
-        System.out.println("Ваш платеж: " + service.calculate(2, 1000_000, 9.99));
-        System.out.println("Ваш платеж: " + service.calculate(3, 1000_000, 9.99));
+        for (int i = 1; i < 4; i++) {
+            System.out.println("Ваш платеж: " + service.calculate(i, 1000_000, 9.99));
+        }
     }
 }
